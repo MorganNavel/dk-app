06771afaaa941f0ca5188c1ddbdbfed7bd767a75
Danbi Park <64319909+DanbeePark9@users.noreply.github.com>
2023-07-02 00:34:17 +0900

    add:header and footer

---
+++ src/index.php
@@ -0,0 +1,6 @@
+<?php 
+ require_once "./php/header.php"
+?>
+<?php 
+// require_once "./php/footer.php"
+?>
--- src/php/signup.php
+++ src/php/signup.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FKLearning</title>
     <script src="./js/form.js" ></script>
-    <link rel="stylesheet" href="./css/style.css">
+    <!-- <link rel="stylesheet" href="./css/style.css"> -->
 </head>
 <body>  
     <form class="form" action="./php/authentification.php" method="post">
