37fbaffd2f758778688cbfdc7b83c5f812531ea9
MorganNavel <navelmorgan34@gmail.com>
2023-07-01 17:47:16 +0200

    modif: html

--- src/index.php
+++ src/index.php
@@ -1,6 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Principal</title>
+</head>
+<body>
 <?php 
  require_once "./php/header.php"
 ?>
 <?php 
-// require_once "./php/footer.php"
+require_once "./php/footer.php"
 ?>
+</body>
+</html>
+
--- src/php/signup.php
+++ src/php/signup.php
@@ -5,11 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FKLearning</title>
     <script src="./js/form.js" ></script>
-    <!-- <link rel="stylesheet" href="./css/style.css"> -->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>  
     <form class="form" action="./php/authentification.php" method="post">
-
         <div class="flex">
             <div><object data="./img/name.svg" width="20" height="20"></object></div><input type="text" name="name" placeholder="Enter your name"><br>
         </div>
