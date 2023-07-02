e631460025cd878d0161f607c28ed1b08c01c2af
MorganNavel <navelmorgan34@gmail.com>
2023-07-02 20:06:19 +0200

    modif: bootstrap grid

--- src/php/header.php
+++ src/php/header.php
@@ -3,13 +3,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></header>
 <body>
     <div class="row">
-        <div class="col-2">
+        <div class="col-sm-2">
             <img src="./img/logo.png" width="120" alt="logo">
         </div>
-        <div class="col-9">
+        <div class="col-sm-10">
             <nav>
                 <ul>
-                    <li class="deroulant"><a href="#">Cours Complets &ensp;</a>
+                    <li class="deroulant"><a href="#">Entraînement &ensp;</a>
                         <ul class="sous">
                             <li><a href="#">Cours</a></li>
                             <li><a href="#">Vocabulaire</a></li>
@@ -24,46 +24,5 @@
         </div>
     </div>
     <hr>
-    
-    <!-- <nav class="navbar">
-        <div class="navbar__logo">
-            <img src="./img/logo.png" width="120" alt="logo">
-        </div>
-        <ul>
-            <li id="li_entrainement" class="deroulant"><a href="">Entraînement</a></li>
-                <ul class = "menu_category-sous">
-                    <li class="category"><a href="#">Cours</a></li>
-                    <li class="category"><a href="#">Vocabulaire</a></li>
-                    <li class="category"><a href="#">Expression</a></li>
-                </ul>
-            <li><a href="#">Participer</a></li>
-            <li><a href="#">Ma page</a></li>
-            <li><a href="#">Login</a></li>
-        <div class="navbar__language">
-            <li>Langue</li>
-            <li>
-                <a href="">
-                    <img src="./img/flag-france.png" width="30" alt="FR">
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <img src="./img/flag-south-korea.png" width="30" alt="FR">
-                </a>
-            </li>
-        </div>
-    </nav> -->
-    <!-- <div class="menu_panel" id="menu_panel">
-        <div class="menu_category">
-            <div class="category"><a href="">Cours</a></div>
-            <div class="category"><a href="">Vocabulaire</a></div>
-            <div class="category"><a href="">Expression</a></div>
-        </div>
-    </div> -->
-   
-
-<!-- <div class="conteneur">
-  <p>Du contenu sous le menu</p>
-</div> -->
 </body>
 
