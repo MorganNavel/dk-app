2688c20d0d7ec1d1df3debf3de991a2a4935a97f
MorganNavel <navelmorgan34@gmail.com>
2023-07-02 03:58:24 +0200

    add: database & user registering

--- src/php/DbManager.php
+++ src/php/DbManager.php
@@ -1,9 +1,11 @@
 <?php
-$config = parse_ini_file(".env");
-define("DB_HOST",$config["DB_HOST"]);
-define("DB_NAME",$config["DB_NAME"]);
-define("DB_USERNAME",$config["DB_USERNAME"]);
-define("DB_PASSWORD",$config["DB_PASSWORD"]);
+
+include './config.php';
+define("DB_NAME",$CONFIG["DB_NAME"]);
+define("DB_HOST",$CONFIG["DB_HOST"]);
+define("DB_USERNAME",$CONFIG["DB_USERNAME"]);
+define("DB_PASSWORD",$CONFIG["DB_PASSWORD"]);
+
 
 class DbManager{
     public const HOST = DB_HOST;
@@ -11,8 +13,34 @@ class DbManager{
     public const USERNAME = DB_USERNAME;
     public const PASSWORD = DB_PASSWORD;
     private static $cnx = null;
-    public function getUsername(){
-        return self::DBNAME;
-    } 
+
+    public static function getConnexion(){
+        if(self::$cnx==null){
+            try{
+                self::$cnx = new PDO("mysql:host=".self::HOST.";dbname=".self::DBNAME, self::USERNAME, self::PASSWORD);
+                self::$cnx ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+                self::$cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch(PDOException $e){
+                echo "Erreur: ".$e->getMessage();
+            }
+        }
+        return self::$cnx;
+    }
+    public static function injectConnexion($DB_NAME){
+        try{
+            self::$cnx = new PDO("mysql:host=".self::HOST.";dbname=".$DB_NAME, self::USERNAME, self::PASSWORD);
+            self::$cnx ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            self::$cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connexion réussi";
+        }
+        catch(PDOException $e){
+            echo "Erreur: ".$e->getMessage();
+        }
+        return self::$cnx;
+    }
 }
 ?>
+
+
+
--- src/php/signup.php
+++ src/php/signup.php
@@ -4,25 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FKLearning</title>
-    <script src="./src/js/form.js" ></script>
-    <link rel="stylesheet" href="./src/css/style.css">
+    <script src="./js/form.js" ></script>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>  
-    <form class="form" action="./src/php/authentification.php" method="post">
+    <form class="form" action="./php/authentification.php" method="post">
 
         <div class="flex">
-            <div><object data="./src/img/name.svg" width="20" height="20"></object></div><input type="text" name="name" placeholder="Enter your name"><br>
+            <!--<div><object data="./img/name.svg" width="20" height="20"></object></div>--><input type="text" name="name" placeholder="Enter your name"><br>
         </div>
 
         <div class="flex">
-            <div><object data="./src/img/email.svg" width="20" height="20"></object></div><input type="email" name="email" placeholder="Enter your email"><br> 
+            <!--<div><object data="./img/email.svg" width="20" height="20"></object></div>--><input type="email" name="email" placeholder="Enter your email"><br> 
         </div>
 
         <div id="div-password" class="flex">
-            <div><object data="./src/img/password.svg" width="20" height="20"></object></div><input type="password" name="password" id="password" placeholder="Enter password"> 
-            <div><object data="./src/img/confirm_password.svg" width="20" height="20"></object></div><input type="password" name="confirm_password" id="confirm-password" placeholder="Confirm password">
+            <!--<div><object data="./img/password.svg" width="20" height="20"></object></div>--><input type="password" name="password" id="password" placeholder="Enter password"><br>
+            <!--<div><object data="./img/confirm_password.svg" width="20" height="20"></object></div>--><input type="password" name="confirm_password" id="confirm-password" placeholder="Confirm password">
         </div>
         <div id="error-div"></div>
+        <br>
         <input type="submit" value="VALIDER" id="submit_btn">
     </form>
 </body>
