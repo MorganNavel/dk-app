bbd75ef382a6e257853b563cdd21f4f32c475c27
MorganNavel <navelmorgan34@gmail.com>
2023-07-02 10:04:36 +0200

    add: login functionnality

--- src/php/login.php
+++ src/php/login.php
@@ -2,14 +2,12 @@
 session_start();
 require_once "./UserManager.php";
 if(!empty($_POST["email"]) && !empty($_POST["password"])){
-
-    if(UserManager::addUser($_POST["email"],$_POST["name"],$_POST["password"])!=-1){
-        $_SESSION["name"] = $_POST["name"];
+    if(UserManager::getUserById($_POST["email"],$_POST["password"])!=-1){
         $_SESSION["email"] = $_POST["email"];
-        $_SESSION["password"] = $_POST["password"];
+        $_SESSION["password"] = sha1($_POST["password"]);
         echo "<h1>Bienvenue ",$_POST["name"],"!</h1>";
     }else{
-        echo "<h1>Adresse email ou nom déjà utilisé</h1>";
+        echo "<h1>Erreur email ou mot de passe incorrect</h1>";
     }
 }else{
     echo "<h1>Veuillez remplir tous les champs</h1>";
