93c2ac74b640a3a1e1e1551cc4d0cf4b6e5a6633
Danbi Park <64319909+DanbeePark9@users.noreply.github.com>
2023-07-02 13:02:18 +0900

    modify:header

--- src/index.php
+++ src/index.php
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Principal</title>
+    <link rel="stylesheet" href="/src/css/header.css">
 </head>
 <body>
-<<<<<<< HEAD
     <?php 
         require_once "./php/header.php"
     ?>
@@ -17,17 +16,6 @@
     <?php 
         require_once "./php/footer.php"
     ?>
-=======
-<?php 
- require_once "./php/header.php"
-?>
-<?php
-require_once "./php/signup.php"
-?>
-<?php 
-require_once "./php/footer.php"
-?>
->>>>>>> f2497c82e5fa3451e3ccecc248abe1100e69f1be
 </body>
 </html>
 
