57c3bfa0c53509ce0cc2bc41690b332b595b5427
Danbi Park <64319909+DanbeePark9@users.noreply.github.com>
2023-07-04 13:39:12 +0900

    modify:header width

--- src/index.php
+++ src/index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Principal</title>
-    <link rel="stylesheet" href="./css/header.css">
 </head>
 <body>
     <?php 
