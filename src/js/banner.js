61b3995afb799e8aed6f77fe9774a0c4197894be
Danbi Park <64319909+DanbeePark9@users.noreply.github.com>
2023-07-06 02:30:47 +0900

    add:arrows on banner

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,12 +1,28 @@
 window.addEventListener("DOMContentLoaded", function (e) {
-    var currentSlide = document.getElementById("slide01").value;
+    // var slideIndex = 1;
+    // showSlides(slideIndex);
 
-    while(true){
-        if (currentSlide=="on"){
-            console.log("on");
-        } else {
-            console.log("off");
-        }
-        return;
-    }
+    // function plusSlides(n) {
+    // showSlides(slideIndex += n);
+    // }
+
+    // function currentSlide(n) {
+    // showSlides(slideIndex = n);
+    // }
+
+    // function showSlides(n) {
+    // var i;
+    // var slides = document.getElementsByClassName("mySlides");
+    // var dots = document.getElementsByClassName("dot");
+    // if (n > slides.length) {slideIndex = 1}    
+    // if (n < 1) {slideIndex = slides.length}
+    // for (i = 0; i < slides.length; i++) {
+    //     slides[i].style.display = "none";  
+    // }
+    // for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    // }
+    // slides[slideIndex-1].style.display = "block";  
+    // dots[slideIndex-1].className += " active";
+    // }
 });
--- src/php/banner.php
+++ src/php/banner.php
@@ -1,5 +1,6 @@
 <header>
 <script src="./js/banner.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
 <link rel="stylesheet" href="./css/banner.css">
 </header>
 <body>
@@ -11,43 +12,67 @@
             <input type="radio" name="slide" id="slide04">
             <ul class="slidelist">
                 <li class="slideitem">
+                    <label for="slide04" class="left">
+                        <i class="fa-solid fa-arrow-left"></i>
+                    </label>
                     <h1>
-                    사랑해 1
+                        사랑해 1
                     </h1>
+                    <label for="slide02" class="right">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </label>
                     <h2>
                         Je t'aime
                     </h2>
                 </li>
                 <li class="slideitem">
+                    <label for="slide01" class="left">
+                        <i class="fa-solid fa-arrow-left"></i>
+                    </label>
                     <h1>
                     사랑해 2
                     </h1>
+                    <label for="slide03" class="right">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </label>
                     <h2>
                         Je t'aime
                     </h2>
                 </li>
                 <li class="slideitem">
+                    <label for="slide02" class="left">
+                        <i class="fa-solid fa-arrow-left"></i>
+                    </label>
                     <h1>
                     사랑해 3
                     </h1>
+                    <label for="slide04" class="right">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </label>
                     <h2>
                         Je t'aime
                     </h2>
                 </li>
                 <li class="slideitem">
+                    <label for="slide03" class="left">
+                        <i class="fa-solid fa-arrow-left"></i>
+                    </label>
                     <h1>
-                    사랑해 4
+                        사랑해 4
                     </h1>
+                    <label for="slide01" class="right">
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </label>
                     <h2>
                         Je t'aime
                     </h2>
                 </li>
             </ul>
             <div class="div_dots">
-                <span class="dot1">●</span> 
-                <span class="dot2">●</span> 
-                <span class="dot3">●</span> 
-                <span class="dot4">●</span> 
+                <span class="dot" onclick="currentSlide(1)">●</span> 
+                <span class="dot" onclick="currentSlide(2)">●</span> 
+                <span class="dot" onclick="currentSlide(3)">●</span> 
+                <span class="dot" onclick="currentSlide(4)">●</span> 
             </div>
         </div>
     </div>
