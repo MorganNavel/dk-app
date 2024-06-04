468fbe7997ccfb04e0fb079337c8456b033b682e
MorganNavel <navelmorgan34@gmail.com>
2024-06-04 15:45:12 +0200

    navbar

--- fklearning-app/src/components/Header/Header.tsx
+++ fklearning-app/src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Navbar, Nav, NavDropdown, Container, Image } from "react-bootstrap";
 import avatarDefault from "../../assets/images/avatar-default.png";
 import arrowDown from "../../assets/images/arrow-down.svg";
+import signOutLogo from "../../assets/images/singout.png";
+
 import { User } from "../../types/User";
 
 const Header = () => {
@@ -26,12 +28,14 @@ const Header = () => {
   return (
     <Navbar
       bg="teal"
-      variant="dark"
       expand="lg"
-      className="p-3 bg-teal-700 text-white text-lg"
+      className="p-4 bg-teal-700 text-white text-lg"
     >
       <Container className="flex items-center justify-between">
-        <Navbar.Brand href="/" className="font-bold text-2xl ml-8">
+        <Navbar.Brand
+          href="/"
+          className="font-bold text-2xl ml-8"
+        >
           FKLearning
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -42,26 +46,29 @@ const Header = () => {
           <Nav className="flex space-x-8">
             <Nav.Link
               href="/catalog"
-              className="hover:border-b-2 hover:border-white"
+              className="group relative inline-block"
             >
-              Vidéo Catalogue
+              Vidéo Catalogue{" "}
+              <span className="rounded block max-w-0 group-hover:max-w-full transition-all h-1.5 bg-white"></span>
             </Nav.Link>
             <Nav.Link
               href="/pricing"
-              className="hover:border-b-2 hover:border-white"
+              className="group relative inline-block"
             >
-              Tarif des abonnements
+              Tarif des abonnements{" "}
+              <span className="rounded block max-w-0 group-hover:max-w-full transition-all h-1.5 bg-white"></span>
             </Nav.Link>
             <Nav.Link
               href="/level-test"
-              className="hover:border-b-2 hover:border-white"
+              className="group relative inline-block"
             >
-              Level Test
+              Level Test{" "}
+              <span className="rounded block max-w-0 group-hover:max-w-full transition-all h-1.5 bg-white "></span>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
         {user.idUser !== -1 ? (
-          <Nav className="relative group ml-auto mr-8">
+          <Nav className="relative group mr-14">
             <NavDropdown
               title={
                 <div className="flex items-center cursor-pointer">
@@ -86,7 +93,7 @@ const Header = () => {
               id="basic-nav-dropdown"
               className="relative"
             >
-              <div className="absolute right-0 mt-2 w-48 bg-white text-teal-700 shadow-lg rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
+              <div className="left-1 absolute right-0 mt-2 w-40 bg-white text-teal-700 shadow-lg rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
                 <NavDropdown.Item
                   href="/my-account"
                   className="flex items-center px-4 py-2 hover:bg-teal-700 hover:text-white"
@@ -112,21 +119,17 @@ const Header = () => {
                   className="flex items-center px-4 py-2 hover:bg-teal-700 hover:text-white"
                 >
                   Sign Out
+                  <Image
+                    src={signOutLogo}
+                    width="20"
+                    height="20"
+                    className="ml-6  tint-teal-700"
+                  />
                 </NavDropdown.Item>
               </div>
             </NavDropdown>
           </Nav>
-        ) : (
-          <Nav className="flex space-x-2 ml-auto">
-            {" "}
-            <Nav.Link
-              href="/sign-in"
-              className="border border-white px-3 py-1 rounded hover:bg-white hover:text-teal-700 transition"
-            >
-              Sign In
-            </Nav.Link>
-          </Nav>
-        )}
+        ) : null}
       </Container>
     </Navbar>
   );
