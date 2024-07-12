f23b89237abf87941c67da51c74020ae65727ac0
Morgan <mn@montpellier-engineering.com>
2024-07-12 17:45:41 +0200

    fix: middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,14 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
-  // A list of all locales that are supported
   locales: ['en', 'fr', 'ko'],
  
-  // Used when no locale matches
   defaultLocale: 'en'
 });
  
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(fr|en|ko)/:path*']
+  matcher: ['/((?!api|_next|favicon.ico).*)', '/']
 };
