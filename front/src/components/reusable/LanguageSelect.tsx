72435459e6bcbe4616eb20a31ed9b22c396cfa3b
Morgan NAVEL <navelmorgan34@gmail.com>
2024-09-08 15:21:18 +0200

    LangueSelect.tsx: remove comments

--- front/src/components/reusable/LanguageSelect.tsx
+++ front/src/components/reusable/LanguageSelect.tsx
@@ -5,20 +5,19 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@ui/select"; // Les composants du `shadcn/ui`
-import { useTranslations } from "next-intl"; // Utilisation pour les traductions
-import { usePathname, useRouter } from "next/navigation"; // Navigation Next.js
-import Image from "next/image"; // Utilisation de `next/image` pour les drapeaux
-import frFlag from "@public/assets/img/fr.svg"; // Drapeaux
+} from "@ui/select";
+import { useTranslations } from "next-intl";
+import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
+import frFlag from "@public/assets/img/fr.svg";
 import enFlag from "@public/assets/img/en.svg";
 import koFlag from "@public/assets/img/ko.svg";
-import { get } from "http";
 
 export const LanguageSelect = () => {
-  const t = useTranslations(); // Hook pour traductions
-  const router = useRouter(); // Hook pour la navigation
-  const pathname = usePathname(); // Obtenir l'URL actuelle
-  const currentLang = pathname.split("/")[1]; // Récupérer la langue actuelle depuis l'URL
+  const t = useTranslations();
+  const router = useRouter();
+  const pathname = usePathname();
+  const currentLang = pathname.split("/")[1];
   const getIcon = (lang: string) => {
     switch (lang) {
       case "fr":
@@ -35,11 +34,10 @@ export const LanguageSelect = () => {
   return (
     <Select
       onValueChange={(lng) => {
-        router.replace(`/${lng}${pathname.substring(3)}`); // Remplacer la langue dans l'URL et rediriger
+        router.replace(`/${lng}${pathname.substring(3)}`);
       }}
     >
       <SelectTrigger className='bg-white border border-gray-300 rounded-md'>
-        {/* Valeur actuelle de la langue avec drapeau */}
         <SelectValue
           placeholder={
             <div className='flex items-center space-x-2'>
