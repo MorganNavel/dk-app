e2595728f77de0838e440be6ab7820ee601e4d65
Morgan NAVEL <navelmorgan34@gmail.com>
2024-09-15 11:15:50 +0200

    delete: initialisation db

--- back/src/storage/initDb.ts
+++ back/src/storage/initDb.ts
@@ -5,7 +5,6 @@ import { initUser, User } from "@/models/UserModel";
 import { initLesson, Lesson } from "@/models/LessonModel";
 import { initPricing, Pricing } from "@/models/PricingModel";
 import { Booking, initBooking } from "@/models/BookingModel";
-import bcrypt from "bcrypt";
 
 dotenv.config();
 const ENV = process.env.NODE_ENV || "development";
@@ -72,48 +71,7 @@ Lesson.belongsTo(User, { foreignKey: "idTeacher" });
 async function connectToDb() {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({ force: true });
-
-    const teacher = await User.create({
-      name: "Park",
-      firstname: "Danbee",
-      email: "ilovelanguages@naver.com",
-      password_hash: bcrypt.hashSync("26122002", 10),
-      role: "teacher",
-      languages: "French;English;Korean",
-      nationality: "Korean",
-      description: "I am a language lover",
-    });
-    const pricing = await Pricing.create({
-      price: 10,
-      currency: "USD",
-      nbLessons: 4,
-    });
-    const user = await User.create({
-      name: "Navel",
-      firstname: "Morgan",
-      email: "morgan.navel@example.com",
-      password_hash: bcrypt.hashSync("26122002", 10),
-      role: "student",
-      languages: "French;English",
-      nationality: "French",
-      description: "I am a language lover",
-    });
-    const lesson = await Lesson.create({
-      title: "French for beginners",
-      description: "Learn French from scratch",
-      startDate: new Date(),
-      duration: 60,
-      idTeacher: teacher.dataValues.idUser,
-    });
-
-    await Booking.create({
-      idUser: user.dataValues.idUser,
-      idLesson: lesson.dataValues.idLesson,
-      startDate: new Date(),
-      duration: 60,
-      tarif: 10,
-    });
+    await sequelize.sync({ force: false });
 
     console.log("Connected to database");
   } catch (error) {
