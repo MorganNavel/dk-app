d64fa1b349f433b79d851b88d1126d0d4eb5d29e
MorganNavel <navelmorgan34@gmail.com>
2024-10-19 01:41:48 +0200

    refactor: encapsulate transporter to avoid recreation

---
+++ back/src/utils/Transporter.ts
@@ -0,0 +1,35 @@
+
+import nodemailer from "nodemailer";
+import dotenv from "dotenv"
+dotenv.config()
+
+export class Transporter {
+    private static instance: Transporter | null;
+    private static transporter: any;
+    private constructor() {
+        if(!Transporter.instance) {
+            Transporter.instance = this;
+        }
+        return Transporter.instance;
+    }
+
+    private createTransporter() {
+        Transporter.transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: process.env.GMAIL_EMAIL,
+              pass: process.env.GMAIL_PASSWORD
+            }
+          });
+          return Transporter.transporter;
+    }
+    static getTransporter() {
+        if(!Transporter.instance) {
+            Transporter.instance = new Transporter();
+            return Transporter.instance.createTransporter();
+        };
+        return Transporter.transporter;
+    }
+}
+
+
