5aa50f6362314b10e85e904d6cbcba380cadbe4d
MorganNavel <navelmorgan34@gmail.com>
2025-01-26 14:07:59 +0100

    update: change profile to a provider

--- front/src/providers/ReactQueryProvider.tsx
+++ front/src/providers/ReactQueryProvider.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { ProfileProvider } from "@/providers/Profile";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 
 export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ProfileProvider>{children}</ProfileProvider>
+    </QueryClientProvider>
   );
 };
