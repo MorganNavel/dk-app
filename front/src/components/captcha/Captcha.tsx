957e36ac96194d73eb8f8c564e5b75c61da3e2a0
Morgan <mn@montpellier-engineering.com>
2025-02-11 15:56:08 +0100

    debug delete print key

--- front/src/components/captcha/Captcha.tsx
+++ front/src/components/captcha/Captcha.tsx
@@ -7,10 +7,7 @@ interface ReCAPTCHAProps {
 }
 export function Captcha({ onChange }: Readonly<ReCAPTCHAProps>) {
   const locale = useLocale();
-  useEffect(() => {
-    console.log("Captcha rendered");
-    console.log(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY);
-  }, []);
+
   return (
     <ReCAPTCHA
       sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
