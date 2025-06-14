5fe9f73d1d09dc4160df1fd6f9e40e1b24268b0a
MorganNavel <navelmorgan34@gmail.com>
2025-06-14 16:17:30 +0200

    Comment.tsx: modify comment styling

--- front/src/components/reusable/Comment.tsx
+++ front/src/components/reusable/Comment.tsx
@@ -9,19 +9,19 @@ interface CommentProps {
 
 export const Comment = ({ fullname, comment, rating }: CommentProps) => {
   return (
-    <Card className="p-6 shadow-lg rounded-2xl bg-background ">
-      <CardHeader className="text-center text-lg font-semibold">
+    <Card className='relative max-w-96 lg:max-w-128 h-full cursor-pointer overflow-hidden rounded-xl border p-4'>
+      <CardHeader className='text-center text-lg font-semibold'>
         - {fullname} -
       </CardHeader>
       <CardContent>
-        <p className="text-justify text-md lg:text-xl min-h-[350px]">
+        <p className="relative px-5 italic before:content-['“'] before:absolute before:left-0 before:top-0 before:text-2xl before:text-gray-400 after:content-['”'] after:absolute after:right-0 after:bottom-0 after:text-2xl after:text-gray-400 ">
           {comment}
         </p>
       </CardContent>
       <CardFooter>
-        <div className="flex justify-center gap-1 mt-3 lg:mt-4">
+        <div className='flex justify-center gap-1 mt-3 lg:mt-4'>
           {renderStars(rating).map((star, index) => (
-            <span key={index} className="text-yellow-500 text-lg">
+            <span key={index} className='text-yellow-500 text-lg'>
               {star}
             </span>
           ))}
