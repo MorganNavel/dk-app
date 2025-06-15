1789299577adbe7322bcb79c1fac273d344214c9
MorganNavel <navelmorgan34@gmail.com>
2025-06-15 19:06:46 +0200

    fix: change import from next/link to i18n/routing link for taking into consideration the language

--- front/src/components/home/carrousel/items/StartKoreanJourney.tsx
+++ front/src/components/home/carrousel/items/StartKoreanJourney.tsx
@@ -1,7 +1,6 @@
 import { useTranslations } from "next-intl";
 import { ItemLayout } from "./ItemLayout";
 import rocket from "@public/assets/img/rocket.png";
-import { BadgeCheck, Star, Sparkles, Headset } from "lucide-react";
 import { useRouter } from "@/i18n/routing";
 import { IoAccessibilityOutline } from "react-icons/io5";
 
@@ -17,7 +16,7 @@ export const StartKoreanJourneyComponent = () => {
       imageAlt={"rocket"}
       width={448}
       height={448}
-      onClick={() => router.push("/sign-up")}
+      onClick={() => router.push("/auth/sign-up")}
       tags={
         <div className='flex items-center gap-2 text-emerald-500 font-medium'>
           <IoAccessibilityOutline className='w-5 h-5' /> {t("tags.accessible")}
