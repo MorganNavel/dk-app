086badea412d5784b804994bc0b108b14c7c951b
MorganNavel <navelmorgan34@gmail.com>
2025-06-23 00:38:52 +0200

    remove apiCall imports

--- front/src/app/[locale]/danbee-park/schedule/actions.ts
+++ front/src/app/[locale]/danbee-park/schedule/actions.ts
@@ -1,15 +1,24 @@
 "use server";
+
+import { redirect } from "@/i18n/routing";
+
 export async function createBooking(idLesson: number) {
-  const response = await fetch(`/api/lesson/${idLesson}/booking`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await fetch(
+    `http://localhost:3000/api/lesson/${idLesson}/booking`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   if (!response.ok) {
     const errorData = await response.json();
-    throw new Error(errorData.key ?? "Booking failed");
+    if (errorData.redirectTo) {
+      redirect({ href: errorData.redirectTo, locale: "fr" });
+      throw new Error(`Redirection to ${errorData.redirectTo} failed`);
+    }
   }
 
   return await response.json();
