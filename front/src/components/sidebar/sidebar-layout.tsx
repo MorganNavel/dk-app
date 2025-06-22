9686d0a67f3d68ff4ef3e710ae7a260dc15b1a20
MorganNavel <navelmorgan34@gmail.com>
2025-06-23 00:06:48 +0200

    add breadcrumb to sidebar layout

--- front/src/components/sidebar/sidebar-layout.tsx
+++ front/src/components/sidebar/sidebar-layout.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import { SidebarInset, SidebarTrigger, useSidebar } from "../ui/sidebar";
 import { AppSidebar } from "./app-sidebar";
 import { Separator } from "../ui/separator";
+import { DynamicBreadcrumb } from "./dynamic-breadcrumb";
 
 export const SidebarLayout = ({ children }: { children: ReactNode }) => {
   const { open } = useSidebar();
@@ -15,10 +16,12 @@ export const SidebarLayout = ({ children }: { children: ReactNode }) => {
           open ? " w-[calc(100%-16rem)]" : " w-[calc(100%-4rem)]"
         }`}
       >
-        <header className=' fixed flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
+        <header className='fixed top-0 z-50 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 bg-sidebar w-full border-b border-border'>
           <div className='flex items-center gap-2 px-4'>
             <SidebarTrigger className='-ml-1 z-[9999]' />
             <Separator orientation='vertical' className='mr-2 h-4' />
+
+            <DynamicBreadcrumb />
           </div>
         </header>
         {children}
