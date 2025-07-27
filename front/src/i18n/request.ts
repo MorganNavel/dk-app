5d5d592ccfea5ae4eaa9ad63226091718b7e7fe4
MorganNavel <navelmorgan34@gmail.com>
2025-07-27 18:33:52 +0200

    The `locale` parameter in `getRequestConfig` is deprecated

--- front/src/i18n/request.ts
+++ front/src/i18n/request.ts
@@ -1,11 +1,12 @@
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
-import { notFound } from "next/navigation";
 
-export default getRequestConfig(async ({locale}) => {
-  // This typically corresponds to the `[locale]` segment
-  if(!routing.locales.includes(locale as any)) return notFound();
-
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale;
+  // Ensure that the incoming locale is valid
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
   return {
     locale,
     messages: (await import(`../../messages/${locale}.json`)).default,
