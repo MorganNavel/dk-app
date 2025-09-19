4e7e9c8cf80b883eebb7313c3da4bfeefb7c0fe2
MorganNavel <navelmorgan34@gmail.com>
2025-09-19 02:07:15 +0200

    fix: hide dashboard when logging out

--- front/src/app/[locale]/danbee-park/dashboard/dashboard.tsx
+++ front/src/app/[locale]/danbee-park/dashboard/dashboard.tsx
@@ -7,8 +7,8 @@ export function DashboardLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const user = useSession();
-  if (user.isPending) return <div className='min-h-screen' />;
-  if (!user || user.data?.user.role !== "teacher") notFound();
+  if (user.isPending || !user || user.data?.user.role !== "teacher")
+    return <div className='min-h-screen' />;
   return (
     <div className='mx-5 lg:mx-15 min-h-screen pt-32 flex flex-col gap-6'>
       {children}
